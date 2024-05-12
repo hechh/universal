d90c50fecb8bc5ba9a14fc204b4fdd597c8769a8
chenghuihe <chenghuihe@aladinfun.com>
2024-05-12 21:29:48 +0800

    [add] 保存代码

--- cmd/game/main.go
+++ cmd/game/main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"flag"
+	"universal/common/pb"
+	"universal/framework"
+	"universal/server/game"
+)
+
 func main() {
+	var serverId int
+	var lpath, ypath string
+	flag.IntVar(&serverId, "id", 0, "ip:port地址")
+	flag.StringVar(&lpath, "log", "", "日志输出目录")
+	flag.StringVar(&ypath, "yaml", "", "日志输出目录")
+	flag.Parse()
+	// 设置全局变量
+	framework.SetGlobal(serverId, pb.ClusterType_GAME)
+	game.Run(lpath, ypath)
 }
--- server/game/game.go
+++ server/game/game.go
@@ -1,11 +1,5 @@
 package game
 
-import "flag"
-
-func Run() {
-	var addr, logPath string
-	flag.StringVar(&addr, "addr", "", "ip:port地址")
-	flag.StringVar(&logPath, "log", "", "日志输出目录")
-	flag.Parse()
+func Run(lpath, ypath string) {
 
 }
