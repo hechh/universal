150da8919e81052d3e074567b90cce5a2bee54f7
unknown <Administrator@DESKTOP-RMTUH81.q1oa.com>
2024-06-19 20:04:47 +0800

    [add] 保存代码

--- framework/library/encoding/integer.go
+++ framework/library/encoding/integer.go
@@ -1,91 +1,105 @@
 package encoding
 
-func Uint16Encode(value uint16) (buf []byte) {
-	if value>>14 != 0 {
-		buf = append(buf, byte(((DataTypeUint16<<4)&0xf0)|(DataType(WireTypeVariant)&0x0f)))
-		buf = append(buf, byte(value&0xff), byte((value>>8)&0xff))
-	} else {
-		buf = append(buf, byte(((DataTypeUint16<<4)&0xf0)|(DataType(WireType16Bit)&0x0f)))
-		buf = append(buf, VariantUint64Encode(uint64(value))...)
+/*
+* |dddd 0000|--1byte--| 		int8、uint8类型：无编码类型。
+* |dddd xx00|--<=2byte--|		int16、uint16类型：当val>>14 等于0时, 采用variant编码，否则采用16bit编码
+* |dddd xx00|--<=4byte--|		int32、uint32类型：当val>>28 等于0时，采用variant编码，否则采用32bit编码
+* |dddd xx00|--<=8byte--|		int64、uint64类型：当val>>56 等于0时，采用variant编码，否则采用64bit编码
+ */
+
+func IntegerEncode(vv interface{}) (buf []byte) {
+	switch value := vv.(type) {
+	case int16:
+		val := uint16((value << 1) ^ (value >> 15))
+		if val>>14 != 0 {
+			buf = append(buf, byte(((DataTypeInt16<<4)&0xf0)|(DataType(WireTypeVariant)&0x0f)))
+			buf = append(buf, byte(val&0xff), byte((val>>8)&0xff))
+		} else {
+			buf = append(buf, byte(((DataTypeInt16<<4)&0xf0)|(DataType(WireType16Bit)&0x0f)))
+			buf = append(buf, VariantUint64Encode(uint64(val))...)
+		}
+	case int32:
+		val := uint32((value << 1) ^ (value >> 31))
+		if val>>28 != 0 {
+			buf = append(buf, byte(((DataTypeInt32<<4)&0xf0)|(DataType(WireTypeVariant)&0x0f)))
+			buf = append(buf, byte(val&0xff), byte((val>>8)&0xff), byte((val>>16)&0xff), byte((val>>24)&0xff))
+		} else {
+			buf = append(buf, byte(((DataTypeInt32<<4)&0xf0)|(DataType(WireType32Bit)&0x0f)))
+			buf = append(buf, VariantUint64Encode(uint64(val))...)
+		}
+	case int64:
+		val := uint64((value << 1) ^ (value >> 63))
+		if val>>56 != 0 {
+			buf = append(buf, byte(((DataTypeInt64<<4)&0xf0)|(DataType(WireTypeVariant)&0x0f)))
+			buf = append(buf, byte(val&0xff), byte((val>>8)&0xff), byte((val>>16)&0xff), byte((val>>24)&0xff), byte((val>>32)&0xff), byte((val>>40)&0xff), byte((val>>48)&0xff), byte((val>>56)&0xff))
+		} else {
+			buf = append(buf, byte(((DataTypeInt64<<4)&0xf0)|(DataType(WireType64Bit)&0x0f)))
+			buf = append(buf, VariantUint64Encode(uint64(val))...)
+		}
+	case uint16:
+		if value>>14 != 0 {
+			buf = append(buf, byte(((DataTypeUint16<<4)&0xf0)|(DataType(WireTypeVariant)&0x0f)))
+			buf = append(buf, byte(value&0xff), byte((value>>8)&0xff))
+		} else {
+			buf = append(buf, byte(((DataTypeUint16<<4)&0xf0)|(DataType(WireType16Bit)&0x0f)))
+			buf = append(buf, VariantUint64Encode(uint64(value))...)
+		}
+	case uint32:
+		if value>>28 != 0 {
+			buf = append(buf, byte(((DataTypeUint32<<4)&0xf0)|(DataType(WireTypeVariant)&0x0f)))
+			buf = append(buf, byte(value&0xff), byte((value>>8)&0xff), byte((value>>16)&0xff), byte((value>>24)&0xff))
+		} else {
+			buf = append(buf, byte(((DataTypeUint32<<4)&0xf0)|(DataType(WireType32Bit)&0x0f)))
+			buf = append(buf, VariantUint64Encode(uint64(value))...)
+		}
+	case uint64:
+		if value>>56 != 0 {
+			buf = append(buf, byte(((DataTypeUint64<<4)&0xf0)|(DataType(WireTypeVariant)&0x0f)))
+			buf = append(buf, byte(value&0xff), byte((value>>8)&0xff), byte((value>>16)&0xff), byte((value>>24)&0xff), byte((value>>32)&0xff), byte((value>>40)&0xff), byte((value>>48)&0xff), byte((value>>56)&0xff))
+		} else {
+			buf = append(buf, byte(((DataTypeUint64<<4)&0xf0)|(DataType(WireType64Bit)&0x0f)))
+			buf = append(buf, VariantUint64Encode(uint64(value))...)
+		}
 	}
 	return
 }
 
-func Int16Encode(val int16) (buf []byte) {
-	var value uint16
-	if value <= 0 {
-		value = ZigzagInt16Encode(val)
-	} else {
-		value = uint16(val)
+func IntegerDecode(buf []byte) interface{} {
+	wireType := WireType((buf[0] >> 2) & 0x01)
+	switch DataType(buf[0] >> 4) {
+	case DataTypeInt16:
+		if wireType == WireTypeVariant {
+			val := uint16(VariantUint64Decode(buf[1:]))
+			return int16((val >> 1) ^ -(val & 1))
+		}
+		return int16(buf[1] | (buf[2] << 8))
+	case DataTypeInt32:
+		if wireType == WireTypeVariant {
+			val := uint32(VariantUint64Decode(buf[1:]))
+			return int32((val >> 1) ^ -(val & 1))
+		}
+		return int32(buf[1] | (buf[2] << 8) | (buf[3] << 16) | (buf[4] << 24))
+	case DataTypeInt64:
+		if wireType == WireTypeVariant {
+			val := uint64(VariantUint64Decode(buf[1:]))
+			return int64((val >> 1) ^ -(val & 1))
+		}
+		return int64(buf[1] | (buf[2] << 8) | (buf[3] << 16) | (buf[4] << 24) | (buf[5] << 32) | (buf[6] << 40) | (buf[7] << 56))
+	case DataTypeUint16:
+		if wireType == WireTypeVariant {
+			return uint16(VariantUint64Decode(buf[1:]))
+		}
+		return uint16(buf[1] | (buf[2] << 8))
+	case DataTypeUint32:
+		if wireType == WireTypeVariant {
+			return uint32(VariantUint64Decode(buf[1:]))
+		}
+		return uint32(buf[1] | (buf[2] << 8) | (buf[3] << 16) | (buf[4] << 24))
+	case DataTypeUint64:
+		if wireType == WireTypeVariant {
+			return uint64(VariantUint64Decode(buf[1:]))
+		}
+		return uint64(buf[1] | (buf[2] << 8) | (buf[3] << 16) | (buf[4] << 24) | (buf[5] << 32) | (buf[6] << 40) | (buf[7] << 56))
 	}
-
-	// 编码
-	if value>>14 != 0 {
-		buf = append(buf, byte(((DataTypeInt16<<4)&0xf0)|(DataType(WireTypeVariant)&0x0f)))
-		buf = append(buf, byte(value&0xff), byte((value>>8)&0xff))
-	} else {
-		buf = append(buf, byte(((DataTypeInt16<<4)&0xf0)|(DataType(WireType16Bit)&0x0f)))
-		buf = append(buf, VariantUint64Encode(uint64(value))...)
-	}
-	return
-}
-
-func Uint32Encode(value uint32) (buf []byte) {
-	if value>>28 != 0 {
-		buf = append(buf, byte(((DataTypeUint32<<4)&0xf0)|(DataType(WireTypeVariant)&0x0f)))
-		buf = append(buf, byte(value&0xff), byte((value>>8)&0xff), byte((value>>16)&0xff), byte((value>>24)&0xff))
-	} else {
-		buf = append(buf, byte(((DataTypeUint32<<4)&0xf0)|(DataType(WireType32Bit)&0x0f)))
-		buf = append(buf, VariantUint64Encode(uint64(value))...)
-	}
-	return
-}
-
-func Int32Encode(val int32) (buf []byte) {
-	var value uint32
-	if value <= 0 {
-		value = ZigzagInt32Encode(val)
-	} else {
-		value = uint32(val)
-	}
-
-	// 编码
-	if value>>28 != 0 {
-		buf = append(buf, byte(((DataTypeInt32<<4)&0xf0)|(DataType(WireTypeVariant)&0x0f)))
-		buf = append(buf, byte(value&0xff), byte((value>>8)&0xff), byte((value>>16)&0xff), byte((value>>24)&0xff))
-	} else {
-		buf = append(buf, byte(((DataTypeInt32<<4)&0xf0)|(DataType(WireType32Bit)&0x0f)))
-		buf = append(buf, VariantUint64Encode(uint64(value))...)
-	}
-	return
-}
-
-func Uint64Encode(value uint64) (buf []byte) {
-	if value>>56 != 0 {
-		buf = append(buf, byte(((DataTypeUint64<<4)&0xf0)|(DataType(WireTypeVariant)&0x0f)))
-		buf = append(buf, byte(value&0xff), byte((value>>8)&0xff), byte((value>>16)&0xff), byte((value>>24)&0xff), byte((value>>32)&0xff), byte((value>>40)&0xff), byte((value>>48)&0xff), byte((value>>56)&0xff))
-	} else {
-		buf = append(buf, byte(((DataTypeUint64<<4)&0xf0)|(DataType(WireType64Bit)&0x0f)))
-		buf = append(buf, VariantUint64Encode(uint64(value))...)
-	}
-	return
-}
-
-func Int64Encode(val int64) (buf []byte) {
-	var value uint64
-	if value <= 0 {
-		value = ZigzagInt64Encode(val)
-	} else {
-		value = uint64(val)
-	}
-
-	// 编码
-	if value>>56 != 0 {
-		buf = append(buf, byte(((DataTypeInt64<<4)&0xf0)|(DataType(WireTypeVariant)&0x0f)))
-		buf = append(buf, byte(value&0xff), byte((value>>8)&0xff), byte((value>>16)&0xff), byte((value>>24)&0xff), byte((value>>32)&0xff), byte((value>>40)&0xff), byte((value>>48)&0xff), byte((value>>56)&0xff))
-	} else {
-		buf = append(buf, byte(((DataTypeInt64<<4)&0xf0)|(DataType(WireType64Bit)&0x0f)))
-		buf = append(buf, VariantUint64Encode(uint64(value))...)
-	}
-	return
+	return nil
 }
--- framework/test/encoding_test.go
+++ framework/test/encoding_test.go
@@ -1,25 +1,18 @@
 package test
 
 import (
-	"fmt"
 	"testing"
 	"universal/framework/library/encoding"
 )
 
 func TestEncoding(t *testing.T) {
-	// Variant编码与解码示例
-	value := uint64(16512)
-	encoded := encoding.VariantUint64Encode(value)
-	fmt.Println("Variant encoded value:", encoded)
-	val := encoding.VariantUint64Decode(encoded)
-	fmt.Println(value, "Variant decoded value:", val)
-
-	/*
-		// Zigzag编码与解码示例
-		n := int64(-453456421)
-		zigzagEncoded := encoding.ZigzagEncode(n)
-		fmt.Println("Zigzag encoded value:", zigzagEncoded)
-		zigzagDecoded := encoding.ZigzagDecode(zigzagEncoded)
-		fmt.Println("Zigzag decoded value:", zigzagDecoded)
-	*/
+	for i := int64(-1000); i < 1000; i++ {
+		// 编码
+		buf := encoding.IntegerEncode(i)
+		// 解码
+		val := encoding.IntegerDecode(buf)
+		if vv, ok := val.(int64); !ok || vv != i {
+			t.Log(i, ok, val)
+		}
+	}
 }
