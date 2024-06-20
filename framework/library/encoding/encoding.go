2abe01d2ee88d311ae668cc798fe122bcea8e296
unknown <Administrator@DESKTOP-RMTUH81.q1oa.com>
2024-06-20 12:26:24 +0800

    [add] 保存代码

--- framework/library/encoding/encoding.go
+++ framework/library/encoding/encoding.go
@@ -1,18 +1,36 @@
 package encoding
 
+import (
+	"fmt"
+	"math"
+)
+
 /*
 * --------------内网跨服务调用api的编码规范--------------
-* |0001 0000| 					bool类型(1 byte): 无编码类型，第一位bit表示bool值
-* |dddd 0000|--1byte--| 		int8、uint8类型：无编码类型。
-* |dddd 0000|--<=2byte--|		int16、uint16类型：当val>>14 等于0时, 采用variant编码，否则采用16bit编码
-* |dddd xx00|--<=4byte--|		int32、uint32类型：当val>>28 等于0时，采用variant编码，否则采用32bit编码
-* |dddd xx00|--<=8byte--|		int64、uint64类型：当val>>56 等于0时，采用variant编码，否则采用64bit编码
-* |1010 0000|--4byte--|			float32类型：固定编码类型，直接采用32bit编码
-* |1011 0000|--8byte--|			float64类型：固定编码类型，直接采用64bit编码
-* |1100 xxxx|xxxx xxxx|--变长--| []byte类型：无编码类型，12bit的长度，最大长度限制4095
-* |1101 xxxx|xxxx xxxx|--变长--| string类型：无编码类型，12bit的长度，最大长度限制4095
-* |1110 xxxx|xxxx xxxx|--变长--| proto类型：无编码类型，12bit的长度，最大长度限制4095
- */
+* 00 --int8,int16,int32,int64,uint8,uint16,uint32,uint64,bool
+*	00	--表示bool类型
+		000x|	--表示true或false
+*	01	--字节编码
+*	10	--variant编码
+		x	--是否有符号
+*			000|	--uint8
+*			001|	--int8
+*			010|	--uint16
+*			011|	--int16
+*			100|	--uint32
+*			101|	--int32
+*			110|	--uint64
+*			111|	--int64
+*	11	--表示float类型
+		0000 	--float32
+		0001 	--float64
+* 01 --[]byte
+*	x	--表示下一个字节是否为长度字节。0表示5bit表示长度。1表示有13bit长度(8191)
+* 10 --string
+*	x	--表示下一个字节是否为长度字节。0表示5bit表示长度。1表示有13bit长度(8191)
+* 11 --proto
+*	x	--表示下一个字节是否为长度字节。0表示5bit表示长度。1表示有13bit长度(8191)
+*/
 
 type WireType uint64
 
@@ -95,4 +113,157 @@ func VariantUint64Decode(data []byte) (value uint64) {
 		}
 	}
 	return
+}
+
+func BytesToUint64(buf []byte) (val uint64) {
+	for i, item := range buf {
+		val |= (uint64(item) << (i * 8))
+	}
+	return
+}
+
+func Uint64ToBytes(val uint64) (buf []byte) {
+	for ; val != 0; val >>= 8 {
+		buf = append(buf, byte(val&0xff))
+	}
+	return
+}
+
+func Uint64Encode(dataType DataType, wireType WireType, val uint64) (buf []byte) {
+	buf = append(buf, byte(((dataType&0x0f)<<4)|DataType(wireType&0x0f)))
+	switch wireType {
+	case WireTypeVariant:
+		buf = append(buf, VariantUint64Encode(val)...)
+	default:
+		buf = append(buf, Uint64ToBytes(val)...)
+	}
+	return
+}
+
+func Uint64Decode(wireType WireType, buf []byte) (val uint64) {
+	switch wireType {
+	case WireTypeVariant:
+		val = VariantUint64Decode(buf)
+	default:
+		val = BytesToUint64(buf)
+	}
+	return
+}
+
+func Encode(vv interface{}) (buf []byte, err error) {
+	switch value := vv.(type) {
+	case string:
+		/*
+			s := *(*reflect.StringHeader)(unsafe.Pointer(&value))
+			b := &reflect.SliceHeader{Data: s.Data, Len: s.Len, Cap: s.Len}
+			bb := *(*[]byte)(unsafe.Pointer(b))
+		*/
+	case []byte:
+		ll := uint64(len(buf))
+		if limit := uint64(0x1<<12 - 1); ll > limit {
+			err = fmt.Errorf("[]byte out of length limit(%d)", limit)
+			return
+		}
+		buf = append(buf, byte(uint64(DataTypeBytes&0x0f)<<4|ll>>8), byte(ll&0xff))
+		buf = append(buf, value...)
+	case bool:
+		if value {
+			buf = append(buf, byte(DataTypeBool<<4+0x01))
+		} else {
+			buf = append(buf, byte(DataTypeBool<<4))
+		}
+	case uint8:
+		buf = append(buf, byte(DataTypeUint8<<4), byte(value))
+	case int8:
+		buf = append(buf, byte(DataTypeInt8<<4), byte(value))
+	case float32:
+		val := math.Float32bits(value)
+		if val>>28 != 0 {
+			buf = Uint64Encode(DataTypeFloat32, WireType32Bit, uint64(val))
+		} else {
+			buf = Uint64Encode(DataTypeFloat32, WireTypeVariant, uint64(val))
+		}
+	case float64:
+		val := math.Float64bits(value)
+		if val>>56 != 0 {
+			buf = Uint64Encode(DataTypeFloat64, WireType64Bit, val)
+		} else {
+			buf = Uint64Encode(DataTypeFloat64, WireTypeVariant, val)
+		}
+	case int16:
+		val := uint16((value << 1) ^ (value >> 15))
+		if val>>14 != 0 {
+			buf = Uint64Encode(DataTypeInt16, WireType16Bit, uint64(val))
+		} else {
+			buf = Uint64Encode(DataTypeInt16, WireTypeVariant, uint64(val))
+		}
+	case int32:
+		val := uint32((value << 1) ^ (value >> 31))
+		if val>>28 != 0 {
+			buf = Uint64Encode(DataTypeInt32, WireType32Bit, uint64(val))
+		} else {
+			buf = Uint64Encode(DataTypeInt32, WireTypeVariant, uint64(val))
+		}
+	case int64:
+		val := uint64((value << 1) ^ (value >> 63))
+		if val>>56 != 0 {
+			buf = Uint64Encode(DataTypeInt64, WireType64Bit, val)
+		} else {
+			buf = Uint64Encode(DataTypeInt64, WireTypeVariant, val)
+		}
+	case uint16:
+		if value>>14 != 0 {
+			buf = Uint64Encode(DataTypeUint16, WireType16Bit, uint64(value))
+		} else {
+			buf = Uint64Encode(DataTypeUint16, WireTypeVariant, uint64(value))
+		}
+	case uint32:
+		if value>>28 != 0 {
+			buf = Uint64Encode(DataTypeUint32, WireType32Bit, uint64(value))
+		} else {
+			buf = Uint64Encode(DataTypeUint32, WireTypeVariant, uint64(value))
+		}
+	case uint64:
+		if value>>56 != 0 {
+			buf = Uint64Encode(DataTypeUint64, WireType64Bit, value)
+		} else {
+			buf = Uint64Encode(DataTypeUint64, WireTypeVariant, value)
+		}
+	}
+	return
+}
+
+func Decode(buf []byte) interface{} {
+	wireType := WireType(buf[0] & 0x0f)
+	switch DataType((buf[0] >> 4) & 0x0f) {
+	case DataTypeBytes:
+		ll := (buf[0]&0x0f)<<8 | buf[1]
+		return buf[2 : ll+2]
+	case DataTypeBool:
+		return buf[0]&0x01 == 1
+	case DataTypeInt8:
+		return int8(buf[1])
+	case DataTypeUint8:
+		return uint8(buf[1])
+	case DataTypeFloat32:
+		return math.Float32frombits(uint32(Uint64Decode(wireType, buf[1:])))
+	case DataTypeFloat64:
+		return math.Float64frombits(Uint64Decode(wireType, buf[1:]))
+	case DataTypeInt16:
+		val := uint16(Uint64Decode(wireType, buf[1:]))
+		return int16((val >> 1) ^ -(val & 1))
+	case DataTypeInt32:
+		val := uint32(Uint64Decode(wireType, buf[1:]))
+		return int32((val >> 1) ^ -(val & 1))
+	case DataTypeInt64:
+		val := Uint64Decode(wireType, buf[1:])
+		return int64((val >> 1) ^ -(val & 1))
+	case DataTypeUint16:
+		return uint16(Uint64Decode(wireType, buf[1:]))
+	case DataTypeUint32:
+		return uint32(Uint64Decode(wireType, buf[1:]))
+	case DataTypeUint64:
+		return Uint64Decode(wireType, buf[1:])
+	}
+	return nil
 }
--- framework/test/encoding_test.go
+++ framework/test/encoding_test.go
@@ -6,24 +6,29 @@ import (
 )
 
 func TestEncoding(t *testing.T) {
-	t.Run("true", func(t *testing.T) {
-		buf := encoding.Encode(true)
+	t.Run("bool", func(t *testing.T) {
+		buf, _ := encoding.Encode(true)
 		val := encoding.Decode(buf)
 		if vv, ok := val.(bool); !ok || vv != true {
 			t.Log(true, ok, vv)
 		}
-	})
-	t.Run("false", func(t *testing.T) {
-		buf := encoding.Encode(false)
-		val := encoding.Decode(buf)
-		if vv, ok := val.(bool); !ok || vv != false {
+
+		buf02, _ := encoding.Encode(false)
+		val02 := encoding.Decode(buf02)
+		if vv, ok := val02.(bool); !ok || vv != false {
 			t.Log(false, ok, vv)
 		}
+	})
+	t.Run("[]byte", func(t *testing.T) {
+		value := []byte{12, 34, 23, 34, 45, 56}
+		buf, _ := encoding.Encode(value)
+		val := encoding.Decode(buf)
+		t.Log(value, val)
 	})
 	t.Run("float", func(t *testing.T) {
 		for i := float32(-134.123); i < 1000; i++ {
 			// 编码
-			buf := encoding.Encode(i)
+			buf, _ := encoding.Encode(i)
 			// 解码
 			val := encoding.Decode(buf)
 			if vv, ok := val.(float32); !ok || vv != i {
@@ -32,7 +37,7 @@ func TestEncoding(t *testing.T) {
 		}
 		for i := float64(-12134.123234); i < 1000; i++ {
 			// 编码
-			buf := encoding.Encode(i)
+			buf, _ := encoding.Encode(i)
 			// 解码
 			val := encoding.Decode(buf)
 			if vv, ok := val.(float64); !ok || vv != i {
@@ -42,14 +47,14 @@ func TestEncoding(t *testing.T) {
 	})
 	t.Run("int8", func(t *testing.T) {
 		value := int8(-1)
-		buf := encoding.Encode(value)
+		buf, _ := encoding.Encode(value)
 		val := encoding.Decode(buf)
 		if vv, ok := val.(int8); !ok || vv != value {
 			t.Log(value, ok, val)
 		}
 
 		value02 := uint8(0xff)
-		buf02 := encoding.Encode(value02)
+		buf02, _ := encoding.Encode(value02)
 		val02 := encoding.Decode(buf02)
 		if vv, ok := val02.(uint8); !ok || vv != value02 {
 			t.Log(value02, ok, val02)
@@ -57,27 +62,27 @@ func TestEncoding(t *testing.T) {
 	})
 	t.Run("int16", func(t *testing.T) {
 		value := int16(-1)
-		buf := encoding.Encode(value)
+		buf, _ := encoding.Encode(value)
 		val := encoding.Decode(buf)
 		if vv, ok := val.(int16); !ok || vv != value {
 			t.Log(value, ok, val)
 		}
 		value02 := int16(0x09)
-		buf02 := encoding.Encode(value02)
+		buf02, _ := encoding.Encode(value02)
 		val02 := encoding.Decode(buf02)
 		if vv, ok := val02.(int16); !ok || vv != value02 {
 			t.Log(value02, ok, val02)
 		}
 
 		value03 := uint16(12)
-		buf03 := encoding.Encode(value03)
+		buf03, _ := encoding.Encode(value03)
 		val03 := encoding.Decode(buf03)
 		if vv, ok := val03.(uint16); !ok || vv != value03 {
 			t.Log(value03, ok, val03)
 		}
 
 		value04 := uint16(0x09)
-		buf04 := encoding.Encode(value04)
+		buf04, _ := encoding.Encode(value04)
 		val04 := encoding.Decode(buf04)
 		if vv, ok := val04.(uint16); !ok || vv != value04 {
 			t.Log(value04, ok, val04)
@@ -85,27 +90,27 @@ func TestEncoding(t *testing.T) {
 	})
 	t.Run("int32", func(t *testing.T) {
 		value := int32(-1)
-		buf := encoding.Encode(value)
+		buf, _ := encoding.Encode(value)
 		val := encoding.Decode(buf)
 		if vv, ok := val.(int32); !ok || vv != value {
 			t.Log(value, ok, val)
 		}
 		value02 := int32(0x09)
-		buf02 := encoding.Encode(value02)
+		buf02, _ := encoding.Encode(value02)
 		val02 := encoding.Decode(buf02)
 		if vv, ok := val02.(int32); !ok || vv != value02 {
 			t.Log(value02, ok, val02)
 		}
 
 		value03 := uint32(12)
-		buf03 := encoding.Encode(value03)
+		buf03, _ := encoding.Encode(value03)
 		val03 := encoding.Decode(buf03)
 		if vv, ok := val03.(uint32); !ok || vv != value03 {
 			t.Log(value03, ok, val03)
 		}
 
 		value04 := uint32(0x09)
-		buf04 := encoding.Encode(value04)
+		buf04, _ := encoding.Encode(value04)
 		val04 := encoding.Decode(buf04)
 		if vv, ok := val04.(uint32); !ok || vv != value04 {
 			t.Log(value04, ok, val04)
@@ -113,27 +118,27 @@ func TestEncoding(t *testing.T) {
 	})
 	t.Run("int64", func(t *testing.T) {
 		value := int64(-1)
-		buf := encoding.Encode(value)
+		buf, _ := encoding.Encode(value)
 		val := encoding.Decode(buf)
 		if vv, ok := val.(int64); !ok || vv != value {
 			t.Log(value, ok, val)
 		}
 		value02 := int64(0xfffffffffffffff)
-		buf02 := encoding.Encode(value02)
+		buf02, _ := encoding.Encode(value02)
 		val02 := encoding.Decode(buf02)
 		if vv, ok := val02.(int64); !ok || vv != value02 {
 			t.Log(value02, ok, val02)
 		}
 
 		value03 := uint64(0xffffffffffffffff)
-		buf03 := encoding.Encode(value03)
+		buf03, _ := encoding.Encode(value03)
 		val03 := encoding.Decode(buf03)
 		if vv, ok := val03.(uint64); !ok || vv != value03 {
 			t.Log(value03, ok, val03)
 		}
 
 		value04 := uint64(0x09)
-		buf04 := encoding.Encode(value04)
+		buf04, _ := encoding.Encode(value04)
 		val04 := encoding.Decode(buf04)
 		if vv, ok := val04.(uint64); !ok || vv != value04 {
 			t.Log(value04, ok, val04)
