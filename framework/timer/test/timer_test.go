b99d4dce7eef3994d130ab9b14d59ec15bc74faa
unknown <Administrator@DESKTOP-RMTUH81.q1oa.com>
2024-08-23 10:59:26 +0800

    [add] 保存定时器变更

--- framework/timer/test/timer_test.go
+++ framework/timer/test/timer_test.go
@@ -11,7 +11,6 @@ import (
 
 var (
 	count int64
-	tt    = timer.NewTimer()
 )
 
 func Print() {
@@ -22,47 +21,34 @@ func TestMain(m *testing.M) {
 	m.Run()
 }
 
-func BenchmarkTimer(b *testing.B) {
-	print := func() {
-		fmt.Println(util.GetNowUnixSecond(), "----", atomic.AddInt64(&count, 1))
-	}
-	for i := 0; i < b.N; i++ {
-		tt.Insert(timer.NewTask(print, 1*time.Second, true))
-	}
-}
-
 func TestTimer01(t *testing.T) {
-	tt := timer.NewTimer()
 	for i := 0; i < 50000; i++ {
-		tt.Insert(timer.NewTask(func() {
+		timer.Insert(timer.NewTask(func() {
 			fmt.Println(util.GetNowUnixSecond(), "------------>", i)
 		}, 3*time.Second, false))
 	}
 	time.Sleep(7 * time.Second)
-	tt.Stop()
+	timer.Stop()
 }
 
 func TestTimer02(t *testing.T) {
-	tt := timer.NewTimer()
-	tt.Insert(timer.NewTask(Print, 2*time.Second, false))
+	timer.Insert(timer.NewTask(Print, 2*time.Second, false))
 	time.Sleep(5 * time.Second)
-	tt.Stop()
+	timer.Stop()
 }
 
 func TestInsert01(t *testing.T) {
-	tt := timer.NewTimer()
-	tt.Insert(timer.NewTask(Print, 1*time.Millisecond, true))
-	tt.Insert(timer.NewTask(Print, 2*time.Millisecond, true))
-	tt.Insert(timer.NewTask(Print, 68*time.Millisecond, true))
+	timer.Insert(timer.NewTask(Print, 1*time.Millisecond, true))
+	timer.Insert(timer.NewTask(Print, 2*time.Millisecond, true))
+	timer.Insert(timer.NewTask(Print, 68*time.Millisecond, true))
 	time.Sleep(2 * time.Second)
-	tt.Stop()
+	timer.Stop()
 }
 
 func TestInsert02(t *testing.T) {
-	tt := timer.NewTimer()
-	tt.Insert(timer.NewTask(Print, 65*time.Millisecond, true))
-	tt.Insert(timer.NewTask(Print, 66*time.Millisecond, true))
-	tt.Insert(timer.NewTask(Print, 67*time.Millisecond, true))
+	timer.Insert(timer.NewTask(Print, 65*time.Millisecond, true))
+	timer.Insert(timer.NewTask(Print, 66*time.Millisecond, true))
+	timer.Insert(timer.NewTask(Print, 67*time.Millisecond, true))
 	time.Sleep(1 * time.Second)
-	tt.Stop()
+	timer.Stop()
 }
--- framework/timer/timer.go
+++ framework/timer/timer.go
@@ -3,11 +3,29 @@ package timer
 import (
 	"runtime/debug"
 	"sync"
+	"sync/atomic"
 	"time"
 	"universal/framework/basic/async"
 	"universal/framework/basic/util"
 	"universal/framework/plog"
 )
+
+type Task struct {
+	id     uint64        // 唯一id
+	handle func()        // 任务
+	ttl    time.Duration // 定时时长
+	once   bool          // 是否一次性
+	expire int64         // 过期时长
+}
+
+type Wheel struct {
+	cursor   int64          // 处理游标
+	tickSize int64          // tick的bit数
+	tick     int64          // 时间刻度
+	interval int64          // 时间间隔
+	size     int64          // 转盘大小
+	buckets  []*async.Queue // 任务队列
+}
 
 type Timer struct {
 	sync.WaitGroup
@@ -18,9 +36,58 @@ type Timer struct {
 	exitHandle chan struct{} // 退出通知
 }
 
+var (
+	generator uint64
+	object    *Timer = NewTimer()
+)
+
+func Insert(tt *Task) {
+	object.Insert(tt)
+}
+
+func Stop() {
+	object.Stop()
+}
+
+func NewTask(f func(), ttl time.Duration, once bool) *Task {
+	return &Task{
+		id:     atomic.AddUint64(&generator, 1),
+		handle: f,
+		ttl:    ttl,
+		once:   once,
+		expire: util.GetNowTime().Add(ttl).UnixMilli(),
+	}
+}
+
+func NewDelayTask(f func(), ttl time.Duration, once bool, interval time.Duration) *Task {
+	return &Task{
+		id:     atomic.AddUint64(&generator, 1),
+		handle: f,
+		ttl:    ttl,
+		once:   once,
+		expire: util.GetNowTime().Add(interval).UnixMilli(),
+	}
+}
+
+func NewWheel(tick, size int64) *Wheel {
+	ll := (1 << size)
+	rets := make([]*async.Queue, ll)
+	for i := 0; i < ll; i++ {
+		rets[i] = async.NewQueue()
+	}
+	return &Wheel{
+		cursor:   util.GetNowUnixMilli(),
+		tickSize: tick,
+		tick:     1<<tick - 1,
+		interval: 1<<(tick+size) - 1,
+		size:     1<<size - 1,
+		buckets:  rets,
+	}
+}
+
 func NewTimer() *Timer {
 	ret := &Timer{
-		wheels:     [4]*Wheel{NewWheel(1, 6, 7), NewWheel(2, 13, 7), NewWheel(3, 20, 7), NewWheel(4, 27, 7)},
+		wheels:     [4]*Wheel{NewWheel(6, 7), NewWheel(13, 7), NewWheel(20, 7), NewWheel(27, 7)},
 		over:       async.NewQueue(),
 		updateOver: make(chan struct{}, 1),
 		exitRun:    make(chan struct{}, 1),
@@ -36,6 +103,13 @@ func NewTimer() *Timer {
 		plog.Fatal("%v\n stack: %s", err, string(debug.Stack()))
 	}, ret.handle)
 	return ret
+}
+
+func (d *Task) Update(now int64) {
+	ttl := int64(d.ttl / time.Millisecond)
+	for d.expire <= now {
+		d.expire += ttl
+	}
 }
 
 func (d *Timer) Stop() {
@@ -66,7 +140,6 @@ func (d *Timer) Insert(tt *Task) {
 
 // 过滤过期任务执行
 func (d *Timer) run() {
-	wheel := d.wheels[0]
 	tt := time.NewTicker(time.Duration(d.wheels[0].tick) * time.Millisecond)
 	for {
 		select {
@@ -77,14 +150,14 @@ func (d *Timer) run() {
 			now := util.GetNowUnixMilli()
 			for _, wl := range d.wheels {
 				for _, item := range wl.Pop(now) {
-					if wheel.IsExpired(now, item.expire) {
+					if !d.wheels[0].IsExpired(now, item.expire) {
+						d.Insert(item)
+					} else {
 						if !item.once {
 							item.Update(now)
 							d.Insert(item)
 						}
 						overs = append(overs, item)
-					} else {
-						d.Insert(item)
 					}
 				}
 			}
@@ -119,4 +192,46 @@ func (d *Timer) handle() {
 			return
 		}
 	}
+}
+
+func (d *Wheel) GetIndex(val int64) int64 {
+	return (val >> d.tickSize) & d.size
+}
+
+// 判断是否过期
+func (d *Wheel) IsExpired(now, expire int64) bool {
+	return (now >> d.tickSize) > (expire >> d.tickSize)
+}
+
+// 插入任务
+func (d *Wheel) Insert(tt *Task) int {
+	cursor := util.GetNowUnixMilli()
+	if d.IsExpired(cursor, tt.expire) {
+		return -1
+	}
+	if tt.expire-cursor >= d.interval {
+		return 1
+	}
+	// 插入成功
+	//plog.Trace("%d: index: %d, Task--->id: %d, now: %d, expire: %d, ttl: %d, once: %v", d.index, d.GetIndex(tt.expire), tt.id, cursor, tt.expire, tt.ttl, tt.once)
+	d.buckets[d.GetIndex(tt.expire)].Push(tt)
+	return 0
+}
+
+// 获取过期或者需要迁移的任务
+func (d *Wheel) Pop(now int64) (rets []*Task) {
+	cursor := atomic.SwapInt64(&d.cursor, now)
+	begin, end := d.GetIndex(cursor), d.GetIndex(now)
+	if end < begin {
+		end += d.size
+	}
+	// 读取过期
+	//plog.Trace("%d: now: %d, begin: %d, end: %d, count: %d", d.index, now, begin, end, d.buckets[begin].GetCount())
+	for i := begin; i < end; i++ {
+		for tt := d.buckets[i&d.size].Pop(); tt != nil; tt = d.buckets[i&d.size].Pop() {
+			rets = append(rets, tt.(*Task))
+		}
+	}
+	//plog.Trace("%d: now: %d, rets: %d", d.index, now, len(rets))
+	return
 }
