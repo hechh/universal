b46f1813ca043af860f0dc3c51183bec5267a226
unknown <Administrator@DESKTOP-RMTUH81.q1oa.com>
2024-08-28 18:35:03 +0800

    [add] 保存代码

--- common/dao/test/dao_test.go
+++ common/dao/test/dao_test.go
@@ -2,12 +2,36 @@ package test
 
 import (
 	"testing"
+	"universal/common/dao/internal/manager"
 	"universal/common/global"
 )
 
 func TestMain(m *testing.M) {
 	cfg := &global.Config{}
-	global.LoadFile("../../../env/yaml/common.global", cfg)
-	global.LoadFile("../../../env/yaml/game.global", cfg)
+	global.LoadFile("../../../env/yaml/common.yaml", cfg)
+	global.LoadFile("../../../env/yaml/game.yaml", cfg)
+
+	if err := manager.InitRedis(cfg.Redis); err != nil {
+		panic(err)
+		return
+	}
 	m.Run()
+}
+
+func TestRedis(t *testing.T) {
+	cli := manager.GetRedis(1)
+	if cli == nil {
+		return
+	}
+
+	result, err := cli.Get("test-hch")
+	t.Log(result, err)
+
+	if err = cli.Set("test-hch", 123); err != nil {
+		t.Log(err)
+		return
+	}
+
+	result, err = cli.Get("test-hch")
+	t.Log(result, err)
 }
