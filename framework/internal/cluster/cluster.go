d126e8192c8c41a662bb9e5029550a52bb7d240e
hch <hch@192.168.1.4>
2025-06-18 01:44:07 +0800

    保存代码

--- framework/internal/cluster/cluster.go
+++ framework/internal/cluster/cluster.go
@@ -3,13 +3,12 @@ package cluster
 import (
 	"sync"
 	"universal/common/pb"
-	"universal/library/mlog"
 	"universal/library/random"
 )
 
 type pool struct {
 	mutex sync.RWMutex
-	nodes []*pb.Node
+	nodes map[int32]*pb.Node
 }
 
 type Cluster struct {
@@ -19,7 +18,7 @@ type Cluster struct {
 func NewCluster() *Cluster {
 	pools := make(map[pb.NodeType]*pool)
 	for i := pb.NodeType_NodeTypeBegin + 1; i < pb.NodeType_NodeTypeEnd; i++ {
-		pools[i] = new(pool)
+		pools[i] = &pool{nodes: make(map[int32]*pb.Node)}
 	}
 	return &Cluster{pools: pools}
 }
@@ -34,65 +33,44 @@ func (c *Cluster) Get(nodeType pb.NodeType, nodeId int32) *pb.Node {
 	nn := c.pools[nodeType]
 	nn.mutex.RLock()
 	defer nn.mutex.RUnlock()
-	for _, val := range nn.nodes {
-		if val.GetId() == nodeId {
-			return val
-		}
-	}
-	return nil
+	return nn.nodes[nodeId]
 }
 
 func (c *Cluster) Del(nodeType pb.NodeType, nodeId int32) bool {
-	if nn := c.Get(nodeType, nodeId); nn == nil {
-		return false
-	}
-
 	nn := c.pools[nodeType]
 	nn.mutex.Lock()
 	defer nn.mutex.Unlock()
-	j := -1
-	for _, val := range nn.nodes {
-		if val.GetId() == nodeId {
-			mlog.Debugf("删除服务节点：%s", val.String())
-			continue
-		}
-		j++
-		nn.nodes[j] = val
-	}
-	nn.nodes = nn.nodes[:j+1]
+	delete(nn.nodes, nodeId)
 	return true
 }
 
 func (c *Cluster) Add(node *pb.Node) bool {
-	if nn := c.Get(node.Type, node.Id); nn != nil {
-		return false
-	}
-
 	nn := c.pools[pb.NodeType(node.Type)]
 	nn.mutex.Lock()
 	defer nn.mutex.Unlock()
-	for i, item := range nn.nodes {
-		if item.GetId() == node.GetId() {
-			nn.nodes[i] = node
-			return true
-		}
-	}
-	nn.nodes = append(nn.nodes, node)
+	nn.nodes[node.Id] = node
 	return true
+}
+
+func (c *Cluster) gets(nodeType pb.NodeType) (rets []*pb.Node) {
+	nn := c.pools[nodeType]
+	nn.mutex.RLock()
+	defer nn.mutex.RUnlock()
+	for _, item := range nn.nodes {
+		rets = append(rets, item)
+	}
+	return
 }
 
 // 随机获取节点
 func (c *Cluster) Random(nodeType pb.NodeType, seed uint64) *pb.Node {
-	nn := c.pools[nodeType]
-	llen := len(nn.nodes)
+	items := c.gets(nodeType)
+	llen := len(items)
 	if llen <= 0 {
 		return nil
 	}
-
-	nn.mutex.RLock()
-	defer nn.mutex.RUnlock()
 	if seed <= 0 {
-		return nn.nodes[random.Int32n(int32(llen))]
+		return items[random.Int32n(int32(llen))]
 	}
-	return nn.nodes[seed%uint64(llen)]
+	return items[seed%uint64(llen)]
 }
--- framework/internal/service/send.go
+++ framework/internal/service/send.go
@@ -1,29 +1,83 @@
 package service
 
 import (
+	"sync/atomic"
 	"universal/common/pb"
+	"universal/library/encode"
 	"universal/library/uerror"
+
+	"github.com/golang/protobuf/proto"
 )
 
 func Send(head *pb.Head, args ...interface{}) error {
 	if head.Src == nil {
 		return uerror.N(1, int32(pb.ErrorCode_ParamInvalid), "Src为空: %v", head)
 	}
-	srcRouter := tab.Get(head.Src.ActorId)
-	srcRouter.Set(node.Type, node.Id)
-	head.Src.NodeType = node.Type
-	head.Src.NodeId = node.Id
-	head.Src.Router = srcRouter.GetData()
-
-	if head.Dst == nil ||
-		head.Dst.NodeType <= pb.NodeType_NodeTypeBegin ||
-		head.Dst.NodeType >= pb.NodeType_NodeTypeEnd {
+	if head.Src.ActorId <= 0 {
+		return uerror.N(1, int32(pb.ErrorCode_ParamInvalid), "Src.ActorId为空: %v", head)
+	}
+	if head.Dst == nil {
 		return uerror.N(1, int32(pb.ErrorCode_ParamInvalid), "Dst为空: %v", head)
 	}
-
+	if head.Dst.NodeType <= pb.NodeType_NodeTypeBegin || head.Dst.NodeType >= pb.NodeType_NodeTypeEnd {
+		return uerror.N(1, int32(pb.ErrorCode_ParamInvalid), "Dst服务类型不支持: %v", head)
+	}
 	if cls.GetCount(head.Dst.NodeType) <= 0 {
 		return uerror.N(1, int32(pb.ErrorCode_NodeNotFound), "服务节点不存在：%v", head)
 	}
+	if err := dispatcher(head); err != nil {
+		return err
+	}
+	if head.Dst.NodeType == node.Type && head.Dst.NodeId == node.Id {
+		return uerror.N(1, int32(pb.ErrorCode_ParamInvalid), "不能调用当前服务节点: %v", head)
+	}
+	atomic.AddUint32(&head.Reference, 1)
+	buf, err := marshal(args...)
+	if err != nil {
+		return err
+	}
+	return bus.Send(head, buf)
+}
 
+func marshal(args ...interface{}) ([]byte, error) {
+	if len(args) == 1 {
+		switch vv := args[0].(type) {
+		case []byte:
+			return vv, nil
+		case proto.Message:
+			if buf, err := proto.Marshal(vv); err != nil {
+				return nil, err
+			} else {
+				return buf, nil
+			}
+		}
+	}
+	return encode.Encode(args...)
+}
+
+func dispatcher(head *pb.Head) error {
+	srcRouter := tab.Get(head.Src.ActorId)
+	srcRouter.Set(node.Type, node.Id)
+
+	dstRouter := tab.Get(head.Dst.ActorId)
+	dstRouter.Set(node.Type, node.Id)
+
+	if head.Dst.NodeId > 0 {
+		if cls.Get(head.Dst.NodeType, head.Dst.NodeId) == nil {
+			return uerror.N(1, int32(pb.ErrorCode_NodeNotFound), "未找到服务节点: %v", head)
+		}
+	} else {
+		head.Dst.NodeId = dstRouter.Get(head.Dst.NodeType)
+		if cls.Get(head.Dst.NodeType, head.Dst.NodeId) == nil {
+			nn := cls.Random(head.Dst.NodeType, head.Dst.ActorId)
+			head.Dst.NodeId = nn.Id
+		}
+	}
+
+	srcRouter.Set(head.Dst.NodeType, head.Dst.NodeId)
+	dstRouter.Set(head.Dst.NodeType, head.Dst.NodeId)
+
+	head.Src.Router = srcRouter.GetData()
+	head.Dst.Router = dstRouter.GetData()
 	return nil
 }
