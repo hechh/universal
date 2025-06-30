d2aeaa4439f6c46fa31f78e43795b5a7664cdc31
unknown <812417032@qq.com>
2025-06-30 17:52:26 +0800

    保存代码

--- framework/cluster/Init.go
+++ framework/cluster/Init.go
@@ -8,6 +8,8 @@ import (
 	"universal/framework/internal/discovery"
 	"universal/framework/internal/node"
 	"universal/framework/internal/router"
+	"universal/library/mlog"
+	"universal/library/safe"
 	"universal/library/uerror"
 )
 
@@ -24,7 +26,7 @@ func Init(cfg *yaml.Config, nodeType pb.NodeType, nodeId int32) error {
 	if srvCfg == nil {
 		return uerror.N(1, int32(pb.ErrorCode_NodeConfigNotFound), "%s(%d)", nodeType, nodeId)
 	}
-
+	safe.Catch(mlog.Fatalf)
 	self = yaml.GetNode(cfg, nodeType, nodeId)
 	tab = router.NewTable(srvCfg.RotuerTTL)
 	cls = node.NewNode()
@@ -63,26 +65,55 @@ func SetBroadcastHandler(f func(*pb.Head, []byte)) error {
 
 func SetSendHandler(f func(*pb.Head, []byte)) error {
 	return buss.SetSendHandler(self, func(head *pb.Head, body []byte) {
-		if head.Src != nil && head.Src.Router != nil {
-			tabSrc := tab.GetOrNew(head.Src.ActorId)
-			tabSrc.SetData(head.Src.Router).Set(self.Type, self.Id)
-		}
-
-		tab.GetOrNew(head.Dst.ActorId).SetData(head.Dst.Router).Set(self.Type, self.Id)
-
+		UpdateRouter(head)
 		f(head, body)
 	})
 }
 
 func SetReplyHandler(f func(*pb.Head, []byte)) error {
 	return buss.SetReplyHandler(self, func(head *pb.Head, body []byte) {
-		if head.Src != nil && head.Src.Router != nil {
-			tabSrc := tab.GetOrNew(head.Src.ActorId)
-			tabSrc.SetData(head.Src.Router).Set(self.Type, self.Id)
-		}
-
-		tab.GetOrNew(head.Dst.ActorId).SetData(head.Dst.Router).Set(self.Type, self.Id)
-
+		UpdateRouter(head)
 		f(head, body)
 	})
+}
+
+func UpdateRouter(head *pb.Head) {
+	if head.Src != nil && head.Src.Router != nil {
+		tabSrc := tab.GetOrNew(head.Src.ActorId)
+		tabSrc.SetData(head.Src.Router).Set(self.Type, self.Id)
+	}
+	tab.GetOrNew(head.Dst.ActorId).SetData(head.Dst.Router).Set(self.Type, self.Id)
+}
+
+func Dispatcher(head *pb.Head) error {
+	if head.Dst == nil || head.Dst.ActorId <= 0 {
+		return uerror.N(1, int32(pb.ErrorCode_DstNodeRouterIsNil), "%v", head)
+	}
+	if head.Dst.NodeType >= pb.NodeType_NodeTypeEnd || head.Dst.NodeType <= pb.NodeType_NodeTypeBegin {
+		return uerror.N(1, int32(pb.ErrorCode_NodeTypeNotSupported), "%v", head.Dst)
+	}
+	// 业务层直接指定具体节点
+	dstTab := tab.GetOrNew(head.Dst.ActorId).Set(self.Type, self.Id)
+	if head.Dst.NodeId > 0 {
+		if cls.Get(head.Dst.NodeType, head.Dst.NodeId) == nil {
+			return uerror.N(1, int32(pb.ErrorCode_NodeNotFound), "%v", head.Dst)
+		}
+		head.Dst.Router = dstTab.GetData()
+		return nil
+	}
+	// 优先从路由中选择
+	if nodeId := dstTab.Get(head.Dst.NodeType); nodeId > 0 {
+		if nn := cls.Get(head.Dst.NodeType, nodeId); nn != nil {
+			dstTab.Set(nn.Type, nn.Id)
+			head.Dst.Router = dstTab.GetData()
+			return nil
+		}
+	}
+	//从集群中随机获取一个节点
+	if nn := cls.Random(head.Dst.NodeType, head.Dst.ActorId); nn != nil {
+		dstTab.Set(nn.Type, nn.Id)
+		head.Dst.NodeId = nn.Id
+		return nil
+	}
+	return uerror.N(1, int32(pb.ErrorCode_NodeNotFound), "%v", head.Dst)
 }
